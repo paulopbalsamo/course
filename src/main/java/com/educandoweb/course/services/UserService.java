9e17bd8ee34d6707c0f325193e670c2d6d4e356b
paulopbalsamo <paulopbalsamo@gmail.com>
2022-05-20 16:59:25 -0300

    Exception Update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -27,34 +29,37 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
-		
+
 	}
-	
+
 	public void delete(Long Id) {
 		try {
-		repository.deleteById(Id);
-	}catch(EmptyResultDataAccessException e) {
-		throw new ResourceNotFoundException(Id);
-	}catch(DataIntegrityViolationException e) {
-		throw new DatabaseException(e.getMessage());
+			repository.deleteById(Id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(Id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	}
+
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
-	
+
 }
